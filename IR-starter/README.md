20649eb9b4443824dc509a26a8ae5fd6274b47c8
Cullan Luther <cullan.luther@gmail.com>
2018-04-21 19:20:26 -0500

    Added readme to IR starter

---
+++ IR-starter/README.md
@@ -0,0 +1,10 @@
+# IR Sound Control
+
+---
+
+When the ambient noise reaches a particular number it sends a signal to Apple TV and pauses what ever your watching
+
+**Modules:**
+
+- Ambient Noise Sensor
+- Ir emitter/receiver
--- IR-starter/index.js
+++ IR-starter/index.js
@@ -10,28 +10,6 @@ var ambient = ambientlib.use(tessel.port['B']);
 var play = new Buffer('238ceed00258fdda0226f98e0258f98e0226f98e0226fdda0258f98e0226f98e0258f98e0226f98e0258f98e0226f98e0258fdda0226fdda0258fdda0226fdda0258f98e0226f98e0258f98e0226f98e0226f95c0226f98e0258fdda0226f98e0258fdda0226f98e0258f9c00258fdda0226fdda0258f98e0226fdda0258f98e0226f95c0226', 'hex');
 var pause = new Buffer('235aee9e0258fdda0226f98e0258f98e0226f98e0258fdda0226f98e0258f98e0226f98e0258f98e0226f98e0258f98e0226fdda0226fda80226fdda0226fda80226f98e0226f95c0226f98e0226f98e0258f98e0226f95c0226fdda0226f98e0258fdda0226f98e0258f98e0226fdda0258fdda0226f98e0258fdda0226f98e0258f98e0226', 'hex');
 
-// // When we're connected
-// infrared.on('ready', function () {
-//     console.log("Connected to IR!");
-//     // Start sending a signal every three seconds
-//     setInterval(function () {
-//         // Send the signal at 38 kHz
-//         infrared.sendRawSignal(38, play, function (err) {
-//             if (err) {
-//                 console.log("Unable to send signal: ", err);
-//             } else {
-//                 console.log("Signal sent!");
-//             }
-//         });
-//     }, 1000); // Every second
-// });
-
-// // If we get data, print it out
-// infrared.on('data', function (data) {
-//     console.log(data.toString('hex'));
-// });
-
-
 ambient.on('ready', function () {
     // Get points of light and sound data.
     setInterval(function () {
